82f8ab4c6a744f181ddf10bbab131f6e02386c1f
filoquin <filoquin@gmail.com>
2021-06-29 09:04:55 -0300

    [add] Testing de cambio de factura

---
+++ inv_ar/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Test de cambio de factura argentina",
+
+    'summary': """Test de cambio de factura argentina""",
+
+    'description': """
+        Test de cambio de factura argentina
+    """,
+
+    'author': "filoquin",
+    'website': "http://www.sipecu.com.ar",
+
+    'category': 'account',
+    'version': '13.0.0.0.1',
+
+    'depends': ['l10n_ar'],
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+
+}
