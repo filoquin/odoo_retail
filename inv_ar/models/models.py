fcdf47576c1b6688511874fdf5eaa9c720c0335a
filoquin <filoquin@gmail.com>
2021-06-29 12:07:02 -0300

    [fix] agrego reporte por defecto para la localiazcion

--- inv_ar/models/models.py
+++ inv_ar/models/models.py
@@ -1,18 +1,23 @@
-# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, RedirectWarning, ValidationError
+from dateutil.relativedelta import relativedelta
+from lxml import etree
+import logging
+_logger = logging.getLogger(__name__)
 
-# from odoo import models, fields, api
+
+class AccountMove(models.Model):
+
+    _inherit = 'account.move'
 
 
-# class inv_ar(models.Model):
-#     _name = 'inv_ar.inv_ar'
-#     _description = 'inv_ar.inv_ar'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    def _get_name_invoice_report(self, report_xml_id):
+        self.ensure_one()
+        if self.l10n_latam_use_documents and self.company_id.country_id.code == 'AR':
+            custom_report = {
+                'account.report_invoice_document_with_payments': 'inv_ar.report_invoice_document_with_payments',
+                'account.report_invoice_document': 'inv_ar.report_invoice_document',
+            }
+            return custom_report.get(report_xml_id) or report_xml_id
+        return super()._get_name_invoice_report(report_xml_id)
